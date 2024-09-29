a4f600df4568a1b2e0499489475eac108d2f5eaf
thunnie920 <tjdms030920@naver.com>
2024-09-29 19:04:29 +0900

    firebase

--- src/index.js
+++ src/index.js
@@ -4,12 +4,35 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter as Router } from 'react-router-dom'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-librarie'
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCC-C9pDinNGsfEyTKbpMQMS5bYBGyZBtU',
+  authDomain: 'mijangone-994d4.firebaseapp.com',
+  projectId: 'mijangone-994d4',
+  storageBucket: 'mijangone-994d4.appspot.com',
+  messagingSenderId: '836669629773',
+  appId: '1:836669629773:web:2c22c92cbc76719d164304',
+  measurementId: 'G-2NEG49X7GQ',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <Router>
-    <App />
-  </Router>,
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
 )
 
 // If you want to start measuring performance in your app, pass a function
